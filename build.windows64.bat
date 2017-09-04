09eff91a7bb6a555d06431d2b1e8c1a9b2ad8406
Tanguy <tbonneau@digitaleo.com>
2017-09-04 18:51:15 +0200

    Update windows packaging

--- build.windows64.bat
+++ build.windows64.bat
@@ -11,40 +11,43 @@ mkdir %CLEEPDESKTOPPATH%
 mkdir dist
 
 :: pyinstaller
-echo
-echo "Packaging cleepremote..."
-echo "------------------------"
+echo.
+echo Packaging cleepremote...
+echo ------------------------
 pyinstaller --clean --noconfirm --noupx --windowed --debug --log-level INFO cleepremote.spec
 move dist\cleepremote %CLEEPDESKTOPPATH%
 
 :: copy files and dirs
-echo
-echo "Copying release files..."
-echo "------------------------"
-copy /Y html %CLEEPDESKTOPPATH%
-copy /Y LICENCE.txt %CLEEPDESKTOPPATH%
-copy /Y main.js %CLEEPDESKTOPPATH%
-copy /Y package.json %CLEEPDESKTOPPATH%
-copy /Y README.md %CLEEPDESKTOPPATH%
-copy /Y resources %CLEEPDESKTOPPATH%
-echo "Done"
+echo.
+echo Copying release files...
+echo ------------------------
+echo html...
+mkdir %CLEEPDESKTOPPATH%\html
+xcopy /S html %CLEEPDESKTOPPATH%\html
+xcopy LICENCE.txt %CLEEPDESKTOPPATH%\
+xcopy main.js %CLEEPDESKTOPPATH%\
+xcopy package.json %CLEEPDESKTOPPATH%\
+xcopy README.md %CLEEPDESKTOPPATH%\
+mkdir %CLEEPDESKTOPPATH%\resources
+xcopy /S resources %CLEEPDESKTOPPATH%\resources
+echo Done
 
 :: electron-builder
-echo
-echo "Packaging cleepdesktop..."
-echo "-------------------------"
+echo.
+echo Packaging cleepdesktop...
+echo -------------------------
 node_modules\.bin\electron-builder --windows --x64 --projectDir %CLEEPDESKTOPPATH%
 
 #cleaning
-echo
-echo "Finalizing..."
-echo "-------------"
+echo.
+echo Finalizing...
+echo -------------
 ping 127.0.0.1 -n 2 > nul
-move %CLEEPDESKTOPPATH%\dist .
-rmdir -/Q /S build
-rmdir -/Q /S __pycache__
-rmdir -/Q /S cleep/__pycache__
-rmdir -/Q /S cleep/libs/__pycache__
-
-echo "Done"
+mkdir dist
+xcope /S %CLEEPDESKTOPPATH%\dist dist
+rmdir /Q /S build
+rmdir /Q /S __pycache__
+rmdir /Q /S cleep/__pycache__
+rmdir /Q /S cleep/libs/__pycache__
+echo Done
 
