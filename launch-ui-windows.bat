64a9162bb6e8007a0d5765f4efdaa15e14cd8236
Tanguy <tbonneau@digitaleo.com>
2017-09-05 23:22:09 +0200

    Fix comment issue

--- launch-ui-windows.bat
+++ launch-ui-windows.bat
@@ -1,5 +1,5 @@
-#Development Cleepdesktop launcher
-# --norpc: disable cleepremote launch. You need to launch it by yourself
-# --logfile: set file log level (info, warn, debug, error, silly, verbose) or disable it (no)
-# --logconsole: set console log level (info, warn, debug, error, silly, verbose) or disable it (no)
+:: Development Cleepdesktop launcher
+:: --norpc: disable cleepremote launch. You need to launch it by yourself
+:: --logfile: set file log level (info, warn, debug, error, silly, verbose) or disable it (no)
+:: --logconsole: set console log level (info, warn, debug, error, silly, verbose) or disable it (no)
 node_modules\.bin\electron . --norpc
