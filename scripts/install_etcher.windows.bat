0e4f19d5f7645738d40671fcf995650f8f674848
Tanguy <tbonneau@digitaleo.com>
2017-09-13 14:21:47 +0200

    Update etcher scripts

--- scripts/install_etcher.windows.bat
+++ scripts/install_etcher.windows.bat
@@ -1,4 +1,9 @@
 @echo off
+
+::params:
+:: $1 : archive fullpath
+:: $2 : install path
+
 if exist "%2\etcher-cli" rmdir /Q /S %2\etcher-cli
 %2\tools\7z\7za.exe x %1 -o"%2\etcher-cli" -aoa
 xcopy %2\scripts\etcher-cli.windows.bat %2\etcher-cli\
