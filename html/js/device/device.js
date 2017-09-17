8a25c12925926af428384189d473a30ad54dbe1d
Tanguy <tbonneau@digitaleo.com>
2017-09-17 23:57:29 +0200

    Add device page loader

--- html/js/device/device.js
+++ html/js/device/device.js
@@ -9,11 +9,20 @@ var deviceController = function($rootScope, $scope, $stateParams)
     self.shell = require('electron').shell;
     self.deviceUrl = $stateParams.url;
     self.wv = document.getElementById('deviceWv');
+    self.loading = false;
 
     //handle external link
     self.wv.addEventListener('new-window', function(event) {
         event.preventDefault();
         self.shell.openExternal(event.url);
+    });
+    
+    //add loader listener
+    self.wv.addEventListener('did-start-loading', function() {
+        self.loading = true;
+    });
+    self.wv.addEventListener('did-stop-loading', function() {
+        self.loading = false;
     });
 
     //configure webview src
