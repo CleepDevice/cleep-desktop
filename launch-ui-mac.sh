1d31202de7d8e750163654e0c4e7a5a5f7ae7783
Tangb <tanguy.bonneau@gmail.com>
2017-09-24 18:57:37 +0200

    Fix macos issues: mdselect does not work properly. This is a workaround, bug has been posted to electron issues tracking

--- launch-ui-mac.sh
+++ launch-ui-mac.sh
@@ -6,4 +6,4 @@
 # --norpc: disable cleepremote launch. You need to launch it by yourself
 # --logfile: set file log level (info, warn, debug, error, silly, verbose) or disable it (no)
 # --logconsole: set console log level (info, warn, debug, error, silly, verbose) or disable it (no)
-./node_modules/.bin/electron . --norpc
+LANG=en_US.UTF-8 ./node_modules/.bin/electron . --norpc
