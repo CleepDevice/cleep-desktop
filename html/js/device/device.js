f2fd2f6dce24841d5f9275425aa68071417c443e
tangb <tanguy.bonneau@gmail.com>
2018-03-02 11:12:46 +0100

    Update device page loading (not working...)

--- html/js/device/device.js
+++ html/js/device/device.js
@@ -3,7 +3,7 @@ var Cleep = angular.module('Cleep')
 /**
  * Device controller
  */
-var deviceController = function($rootScope, $scope, $stateParams)
+var deviceController = function($rootScope, $scope, $stateParams, $timeout)
 {
     var self = this;
     self.shell = require('electron').shell;
@@ -17,17 +17,23 @@ var deviceController = function($rootScope, $scope, $stateParams)
         self.shell.openExternal(event.url);
     });
     
-    //add loader listener
+    //device loading
     self.wv.addEventListener('did-start-loading', function() {
+        console.log('Start loading device');
         self.loading = true;
     });
+
+    //device loaded
     self.wv.addEventListener('did-stop-loading', function() {
-        self.loading = false;
+        console.log('Finish loading device');
+        $timeout(function() {
+            self.loading = false;
+        }, 2000);
     });
 
     //configure webview src
     self.wvAngular = angular.element(self.wv);
     self.wvAngular.attr('src', $stateParams.url);
 };
-Cleep.controller('deviceController', ['$rootScope', '$scope', '$stateParams', deviceController]);
+Cleep.controller('deviceController', ['$rootScope', '$scope', '$stateParams', '$timeout', deviceController]);
 
