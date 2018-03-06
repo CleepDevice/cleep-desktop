685a5fa957941ef473b4ecaa6b8b01a9c1ffb394
Tanguy BONNEAU <tbonneau@digitaleo.com>
2018-03-06 15:10:11 +0100

    Change --norpc command line option to --nocore

--- scripts/launch-ui-mac.sh
+++ scripts/launch-ui-mac.sh
@@ -3,7 +3,7 @@
 #ELECTRON_ENABLE_LOGGING=1 npm start --interactive --disable-http-cache
 
 #Development Cleepdesktop launcher
-# --norpc: disable cleepremote launch. You need to launch it by yourself
+# --nocore: disable cleepdesktopcore launch. You need to launch it by yourself
 # --logfile: set file log level (info, warn, debug, error, silly, verbose) or disable it (no)
 # --logconsole: set console log level (info, warn, debug, error, silly, verbose) or disable it (no)
-LANG=en_US.UTF-8 ./node_modules/.bin/electron . --norpc
+LANG=en_US.UTF-8 ./node_modules/.bin/electron . --nocore
