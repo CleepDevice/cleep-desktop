e87bf158b35ecac626120ddb0cd75bc5fc937858
tangb <tanguy.bonneau@gmail.com>
2018-03-07 16:52:13 +0100

    Start handling mac64 build

---
+++ config/cleepdesktopcore-mac64.spec
@@ -0,0 +1,37 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+a = Analysis(['cleepdesktopcore.py'],
+             pathex=[],
+             binaries=[],
+             datas=[
+                ('core', 'core'),
+                ('tools/etcher-cli.mac', 'scripts/etcher-cli.mac'),
+                ('tools/install-etcher.mac', 'scripts/install-etcher.mac'),
+                ('tools/cmdlogger-mac32', 'tools/cmdlogger-mac')
+             ],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name='cleepdesktopcore',
+          debug=False,
+          strip=False,
+          upx=False,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=False,
+               name='cleepdesktopcore')
---
+++ scripts/build-mac64.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#env
+CURRENTPATH=`pwd`
+CLEEPDESKTOPPATH=build/cleepdesktop_tree
+
+#clear previous process
+/bin/rm -rf dist/
+/bin/rm -rf build/
+
+#create dirs
+/bin/mkdir -p "$CLEEPDESKTOPPATH"
+/bin/mkdir dist
+
+#pyinstaller
+echo
+echo
+echo "Packaging cleepdesktopcore..."
+echo "-----------------------------"
+/bin/cp config/cleepdesktopcore-mac64.spec cleepdesktopcore-mac64.spec
+/usr/local/bin/pyinstaller --clean --noconfirm --noupx --debug --log-level INFO cleepdesktopcore-mac64.spec
+/bin/rm cleepdesktopcore-mac64.spec
+/bin/mv dist/cleepdesktopcore "$CLEEPDESKTOPPATH"
+
+#copy files and dirs
+echo
+echo
+echo "Copying release files..."
+echo "------------------------"
+/bin/cp -a html "$CLEEPDESKTOPPATH"
+/bin/cp -a LICENCE.txt "$CLEEPDESKTOPPATH"
+/bin/cp -a cleepdesktop.js "$CLEEPDESKTOPPATH"
+/bin/cp -a package.json "$CLEEPDESKTOPPATH"
+/bin/cp -a README.md "$CLEEPDESKTOPPATH"
+/bin/cp -a resources "$CLEEPDESKTOPPATH"
+echo "Done"
+
+#electron-builder
+echo
+echo
+echo "Packaging cleepdesktop..."
+echo "-------------------------"
+if [ "$1" == "publish" ]
+then
+    echo "Publishing cleepdesktop..."
+    echo "--------------------------"
+    GH_TOKEN=$GH_TOKEN_CLEEPDESKTOP node_modules/.bin/electron-builder --mac --x64 --projectDir "$CLEEPDESKTOPPATH" --publish onTagOrDraft
+else
+    echo "Packaging cleepdesktop..."
+    echo "-------------------------"
+    node_modules/.bin/electron-builder --mac --x64 --projectDir "$CLEEPDESKTOPPATH"
+fi
+
+#cleaning
+echo
+echo
+echo "Finalizing..."
+echo "-------------"
+/bin/sleep 1
+/bin/mv "./$CLEEPDESKTOPPATH/dist" .
+/bin/rm -rf build
+/bin/rm -rf __pycache__
+/bin/rm -rf cleep/__pycache__
+/bin/rm -rf cleep/libs/__pycache__
+echo "Done"
+
+echo
+echo "Build result in dist/ folder"
