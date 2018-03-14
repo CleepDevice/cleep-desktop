539de15aa9e0d961952ee14afb70799afa7e73c8
Tangb <tanguy.bonneau@gmail.com>
2018-03-14 18:39:43 +0100

    Add mac wifi libraries

---
+++ core/libs/macwirelessinterfaces.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import time
+import re
+try:
+    from core.libs.console import AdvancedConsole
+except:
+    from console import AdvancedConsole
+try:
+    import core.libs.converters as Converters
+except:
+    import converters as Converters
+
+class MacWirelessInterfaces(AdvancedConsole):
+    """
+    Return list of wireless interface names
+    """
+
+    CACHE_DURATION = 10.0
+    WIFI = u'Wi-Fi'
+    
+    def __init__(self):
+        """
+        Constructor
+        """
+        AdvancedConsole.__init__(self)
+
+        #members
+        self._binary = u'/usr/sbin/networksetup'
+        self._command = u'/usr/sbin/networksetup -listallhardwareports'
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.interfaces = {}
+        self.timestamp = None
+
+    def is_installed(self):
+        """
+        Return True if command binary exists
+
+        Return:
+            bool: True if binary exists
+        """
+        return os.path.exists(self._binary)
+
+    def __refresh(self):
+        """
+        Refresh list of interfaces
+        """
+        #check if refresh is needed
+        if self.timestamp is not None and time.time()-self.timestamp<=self.CACHE_DURATION:
+            self.logger.debug('Don\'t refresh')
+            return
+
+        #execute command
+        results = self.find(self._command, r'(?:Hardware Port:\s+(.*?)[\n\r\0]Device:\s+(.*?)[\n\r\0])', re.UNICODE | re.DOTALL, timeout=5.0)
+        self.logger.debug(results)
+
+        #parse results
+        entries = []
+        for group, groups in results:
+            #filter None values
+            groups = list(filter(None, groups))
+            self.logger.debug('groups=%s' % groups)
+
+            if groups[0].startswith(self.WIFI):
+                #it's wifi interface
+                entries.append(groups[1])
+        self.logger.debug('entries: %s' % entries)
+
+        #save interfaces
+        self.interfaces = entries
+
+        #update timestamp
+        self.timestamp = time.time()
+
+    def get_interfaces(self):
+        """
+        Return all interfaces
+
+        Return:
+            list: list of wifi interfaces
+        """
+        self.__refresh()
+
+        return self.interfaces
+
+if __name__ == '__main__':
+    import pprint
+    pp = pprint.PrettyPrinter(indent=2)
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    m = MacWirelessInterfaces()
+    ints = m.get_interfaces()
+    pp.pprint(ints)
+
+
---
+++ core/libs/macwirelessnetworks.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+try:
+    from core.libs.console import AdvancedConsole
+except:
+    from console import AdvancedConsole
+try:
+    import core.libs.converters as Converters
+except:
+    import converters as Converters
+try:
+    from core.libs.wpasupplicantconf import WpaSupplicantConf
+except:
+    from wpasupplicantconf import WpaSupplicantConf
+
+class MacWirelessNetworks(AdvancedConsole):
+    """
+    Return list of wireless networks
+    """
+
+    CACHE_DURATION = 2.0
+    
+    def __init__(self):
+        """
+        Constructor
+        """
+        AdvancedConsole.__init__(self)
+
+        #members
+        self._binary = u'/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
+        self._command = u'/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --scan'
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.networks = {}
+        self.error = False
+        self.timestamp = None
+        self.__last_scanned_interface = None
+
+    def is_installed(self):
+        """ 
+        Return True if command binary exists
+
+        Return:
+            bool: True if binary exists
+        """
+        return os.path.exists(self._binary)
+
+    def __refresh(self, interface):
+        """
+        Refresh list of networks
+        """
+        #check if refresh is needed
+        if self.timestamp is not None and time.time()-self.timestamp<=self.CACHE_DURATION and self.__last_scanned_interface==interface:
+            self.logger.debug('Don\'t refresh')
+            return
+
+        #execute command
+        self.__last_scanned_interface = interface
+        results = self.find(self._command, r'\s+(.*?)\s+(?:(?:.{2}:){5}.{2})\s+(-\d+)\s+(?:.*)\s+(?:Y|N)\s+.{2}\s+(.*)', timeout=15.0)
+        self.logger.debug(results)
+
+        #handle invalid interface for wifi scanning or disabled interface
+        if len(results)==0 and self.get_last_return_code()!=0:
+            self.networks = {}
+            self.error = True
+            return
+
+        #parse results
+        entries = {}
+        for group, groups in results:
+            #filter None values
+            groups = list(filter(None, groups))
+            self.logger.debug(groups)
+
+            #handle encryption
+            encryption = groups[2].lower()
+            if encryption.find(u'wpa2')!=-1:
+                encryption = WpaSupplicantConf.ENCRYPTION_TYPE_WPA2
+            elif encryption.find(u'wpa')!=-1:
+                encryption = WpaSupplicantConf.ENCRYPTION_TYPE_WPA
+            elif encryption.find(u'wep')!=-1:
+                encryption = WpaSupplicantConf.ENCRYPTION_TYPE_WEP
+            elif encryption.find(u'none')!=-1:
+                encryption = WpaSupplicantConf.ENCRYPTION_TYPE_UNSECURED
+            else:
+                encryption = WpaSupplicantConf.ENCRYPTION_TYPE_UNKNOWN
+
+            #handle signal level
+            signal_level = Converters.dbm_to_percent(int(groups[1]))
+
+            #save entry
+            entries[groups[0]] = {
+                u'interface': interface,
+                u'network': groups[0],
+                u'encryption': encryption,
+                u'signallevel': signal_level
+            }
+        self.logger.debug('entries: %s' % entries)
+
+        #save networks and error
+        self.networks = entries
+        self.error = False
+
+    def has_error(self):
+        """
+        Return True if error occured
+        
+        Return:
+            bool: True if error, False otherwise
+        """
+        return self.error
+
+    def get_networks(self, interface):
+        """
+        Return all wifi networks scanned
+
+        Args:
+            interface (string): interface name
+
+        Returns:
+            dict: dictionnary of found wifi networks::
+                {
+                    network: {
+                        interface (string): interface scanned
+                        network (string): wifi network name
+                        encryption (string): encryption type (TODO)
+                        signallevel (float): wifi signal level
+                    },
+                    ...
+                }
+        """
+        self.__refresh(interface)
+
+        return self.networks
+
+if __name__ == '__main__':
+    import pprint
+    pp = pprint.PrettyPrinter(indent=2)
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    m = MacWirelessNetworks()
+    nets = m.get_networks('en1')
+    pp.pprint(nets)
+
+
