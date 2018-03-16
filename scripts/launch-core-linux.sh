97292ba82db06877d55f44ffec4f866ccd60ba4b
tangb <tanguy.bonneau@gmail.com>
2018-03-16 10:24:12 +0100

    Kill core when launching again during dev

--- scripts/launch-core-linux.sh
+++ scripts/launch-core-linux.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+/usr/bin/pkill -9 -f cleepdesktopcore
 python3 cleepdesktopcore.py 5610 ~/.config/CleepDesktop Settings debug true
--- scripts/launch-core-mac.sh
+++ scripts/launch-core-mac.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 /usr/bin/pkill -9 -f cleepdesktopcore
 python3 cleepdesktopcore.py 5610 ~/Library/Application\ Support/CleepDesktop/ Settings debug true
