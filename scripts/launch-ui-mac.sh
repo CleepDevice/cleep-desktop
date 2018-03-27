0df938ca554c36956615d366a1b614ebd46bc2e8
tangb <tanguy.bonneau@gmail.com>
2018-03-27 11:45:13 +0200

    Add comment on LANG under launch-ui-mac

--- scripts/launch-ui-mac.sh
+++ scripts/launch-ui-mac.sh
@@ -6,4 +6,5 @@
 # --nocore: disable cleepdesktopcore launch. You need to launch it by yourself
 # --logfile: set file log level (info, warn, debug, error, silly, verbose) or disable it (no)
 # --logconsole: set console log level (info, warn, debug, error, silly, verbose) or disable it (no)
+#LANG refer to workaround of issue https://github.com/electron/electron/issues/8876
 LANG=en_US.UTF-8 ./node_modules/.bin/electron . --nocore
