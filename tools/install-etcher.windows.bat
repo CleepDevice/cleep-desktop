093a4ed6014972d2f828bc239c45d289aac0fc0a
Tanguy Bonneau <tanguy.bonneau@gmail.com>
2019-09-09 18:29:26 +0200

    Fix etcher install on windows

--- tools/install-etcher.windows.bat
+++ tools/install-etcher.windows.bat
@@ -7,5 +7,5 @@
 
 if exist "%3\etcher-cli" rmdir /Q /S %3\etcher-cli
 if exist "%3\balena-cli" rmdir /Q /S %3\balena-cli
-%2\tools\7z\7za.exe x %1 -o"%3\balena-cli" -aoa
-copy %2\tools\flash.windows.bat %3\etcher-cli\flash.bat
+%2\tools\7z\7za.exe x %1 -o"%3\" -aoa
+copy %2\tools\flash.windows.bat %3\balena-cli\flash.bat
