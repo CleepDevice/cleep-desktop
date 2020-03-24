61e5a45e6120eb7724a23d53f643eed92b17bc79
Tanguy BONNEAU <tanguy.bonneau@niji.fr>
2020-03-24 21:06:19 +0100

    Remove debug log

--- html/js/services/cleep.js
+++ html/js/services/cleep.js
@@ -47,7 +47,6 @@ var cleepService = function($http, $q, $rootScope, toast, $websocket, logger, se
     self.__websocketReceive = function(event) {
         if( event && event.data && typeof(event.data)==='string' ) {
             //broadcast received data
-            logger.debug('Websocket: ', event);
             var data = JSON.parse(event.data);
             $rootScope.$broadcast(data.event, data.data);
         }
