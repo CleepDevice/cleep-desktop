aaa3c906387c6a6040eef73bece7c4be3357ec34
Tangb <tanguy.bonneau@gmail.com>
2021-06-02 12:01:15 +0200

    Add a way to clear messages. Add max message to limit memory usage

--- html/js/services/monitoring.js
+++ html/js/services/monitoring.js
@@ -6,6 +6,7 @@ angular
 .module('Cleep')
 .service('monitoringService', ['$rootScope', 'logger', function($rootScope, logger) {
     var self = this;
+    self.maxMessages = 100;
     self.messages = [];
 
     $rootScope.$on('monitoring', function(_event, data) {
@@ -16,9 +17,20 @@ angular
         // append at beginning new message
         logger.debug('Monitoring message received:', data);
         self.messages.unshift(data);
+
+        if (self.messages.length > self.maxMessages) {
+            self.messages.pop();
+        }
     });
+
+    /**
+     * Clear received messages
+     */
+    self.clearMessages = function() {
+        self.messages.splice(0, self.messages.length);
+    };
 
     self.init = function() {
         // it does nothing. It is used to load this service at application startup
-    }
+    };
 }]);
