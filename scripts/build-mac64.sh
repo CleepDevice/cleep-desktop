03fbea3514ec4e834509b0a093fd67f1864e1a45
Tangb <tanguy.bonneau@gmail.com>
2022-02-05 18:08:22 +0100

    Fix macos script

--- scripts/build-mac64.sh
+++ scripts/build-mac64.sh
@@ -20,11 +20,11 @@ checkResult() {
 CLEEPDESKTOPPATH=packaging/cleepdesktop_tree
 
 # clear previous process
-/bin/rm -rf dist/
-/bin/rm -rf packaging/
+rm -rf dist/
+rm -rf packaging/
 
 # create dirs
-/bin/mkdir -p "$CLEEPDESKTOPPATH"
+mkdir -p "$CLEEPDESKTOPPATH"
 
 # pyinstaller
 echo
@@ -33,18 +33,18 @@ echo "Packaging cleepdesktopcore..."
 echo "-----------------------------"
 python3 -m pip install -r requirements.txt
 checkResult $? 0 "Failed to install python dependencies"
-/bin/cp config/cleepdesktopcore-mac64.spec cleepdesktopcore-mac64.spec
+cp config/cleepdesktopcore-mac64.spec cleepdesktopcore-mac64.spec
 /usr/local/bin/pyinstaller --workpath packaging --clean --noconfirm --noupx --debug all --log-level INFO cleepdesktopcore-mac64.spec
 checkResult $? 0 "Failed to build core application"
-/bin/rm cleepdesktopcore-mac64.spec
-/bin/mv dist/cleepdesktopcore "$CLEEPDESKTOPPATH"
+rm cleepdesktopcore-mac64.spec
+mv dist/cleepdesktopcore "$CLEEPDESKTOPPATH"
 
 #update npm
 echo
 echo
 echo "Building electron app..."
 echo "------------------------"
-/usr/bin/npm ci
+npm ci
 checkResult $? 0 "Failed to run npm"
 node_modules/.bin/tsc --outDir "$CLEEPDESKTOPPATH"
 checkResult $? 0 "Failed to build electron application"
@@ -55,11 +55,11 @@ echo
 echo
 echo "Copying release files..."
 echo "------------------------"
-/bin/cp -a html "$CLEEPDESKTOPPATH"
-/bin/cp -a resources "$CLEEPDESKTOPPATH"
-/bin/cp -a LICENSE.txt "$CLEEPDESKTOPPATH"
-/bin/cp -a package.json "$CLEEPDESKTOPPATH"
-/bin/cp -a README.md "$CLEEPDESKTOPPATH"
+cp -a html "$CLEEPDESKTOPPATH"
+cp -a resources "$CLEEPDESKTOPPATH"
+cp -a LICENSE.txt "$CLEEPDESKTOPPATH"
+cp -a package.json "$CLEEPDESKTOPPATH"
+cp -a README.md "$CLEEPDESKTOPPATH"
 echo "Done"
 
 # electron-builder
@@ -83,13 +83,13 @@ echo
 echo
 echo "Finalizing..."
 echo "-------------"
-/bin/sleep 1
-/bin/mv "./$CLEEPDESKTOPPATH/dist" .
-/bin/rm -rf packaging
-/bin/rm -rf __pycache__
-/bin/rm -rf core/__pycache__
-/bin/rm -rf core/libs/__pycache__
-/bin/rm -rf core/modules/__pycache__
+sleep 1
+mv "./$CLEEPDESKTOPPATH/dist" .
+rm -rf packaging
+rm -rf __pycache__
+rm -rf core/__pycache__
+rm -rf core/libs/__pycache__
+rm -rf core/modules/__pycache__
 echo "Done"
 
 echo
