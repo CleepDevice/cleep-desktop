b0391ca9ba2a42b93bf56105bdf0617df88974bf
Tangb <tanguy.bonneau@gmail.com>
2022-02-05 18:03:48 +0100

    Properly handle errors in macos script

--- scripts/build-mac64.sh
+++ scripts/build-mac64.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Check command result
+# $1: command result (usually $?)
+# $2: awaited command result
+# $3: error message
+checkResult() {
+    if [ $1 -ne $2 ]
+    then
+        msg=$3
+        if [[ -z "$1" ]]; then
+            msg="see output log"
+        fi
+        echo -e "${RED}Error occured: $msg.${NOCOLOR}"
+        exit 1
+    fi
+}
+
 # env
 CLEEPDESKTOPPATH=packaging/cleepdesktop_tree
 
@@ -16,8 +32,10 @@ echo
 echo "Packaging cleepdesktopcore..."
 echo "-----------------------------"
 python3 -m pip install -r requirements.txt
+checkResult $? 0 "Failed to install python dependencies"
 /bin/cp config/cleepdesktopcore-mac64.spec cleepdesktopcore-mac64.spec
 /usr/local/bin/pyinstaller --workpath packaging --clean --noconfirm --noupx --debug all --log-level INFO cleepdesktopcore-mac64.spec
+checkResult $? 0 "Failed to build core application"
 /bin/rm cleepdesktopcore-mac64.spec
 /bin/mv dist/cleepdesktopcore "$CLEEPDESKTOPPATH"
 
@@ -27,7 +45,9 @@ echo
 echo "Building electron app..."
 echo "------------------------"
 /usr/bin/npm ci
+checkResult $? 0 "Failed to run npm"
 node_modules/.bin/tsc --outDir "$CLEEPDESKTOPPATH"
+checkResult $? 0 "Failed to build electron application"
 echo "Done"
 
 # copy files and dirs
@@ -44,16 +64,18 @@ echo "Done"
 
 # electron-builder
 echo
-echo
+echo "DEBUG cmd param \"$1\""
 if [ "$1" == "publish" ]
 then
     echo "Publishing cleepdesktop..."
     echo "--------------------------"
     GH_TOKEN=$GH_TOKEN_CLEEPDESKTOP node_modules/.bin/electron-builder --mac --x64 --projectDir "$CLEEPDESKTOPPATH" --publish always
+    checkResult $? 0 "Failed to publish cleepdesktop"
 else
     echo "Packaging cleepdesktop..."
     echo "-------------------------"
     node_modules/.bin/electron-builder --mac --x64 --projectDir "$CLEEPDESKTOPPATH"
+    checkResult $? 0 "Failed to package cleepdesktop"
 fi
 
 #cleaning
