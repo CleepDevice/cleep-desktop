f73ae0c89f92454dba0e2ba84eba1157ceb28a5c
Tangb <tanguy.bonneau@gmail.com>
2022-08-05 16:03:04 +0200

    Remove core files

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .version import VERSION
-
-__all__ = ['exception', 'rpcserver', 'utils', 'common']
-__version__ = VERSION
-
--- core/libs/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['ht1632c', 'task', 'console', 'dhcpcd', 'emailprovider', 'fstab', 'pushprovider', 'raspiotconf', 'smsprovider', 'task', 'wpasupplicantconf']
-
-#from raspiot.libs import *
