e396a2fd1b8a32a94284e650007d41a557d85d11
Tanguy Bonneau <tanguy.bonneau@insurlytech.com>
2022-08-29 23:20:05 +0200

    Restore download from manuel install page

--- html/js/install/install.controller.js
+++ html/js/install/install.controller.js
@@ -1,8 +1,8 @@
 angular
 .module('Cleep')
 .controller('installController', ['$rootScope', 'toastService', 'confirmService', 'loggerService', 'updateService', 
-                                'installService', 'modalService',
-function($rootScope, toast, confirm, logger, updateService, installService, modalService) {
+                                'installService', 'modalService', 'downloadService',
+function($rootScope, toast, confirm, logger, updateService, installService, modalService, downloadService) {
     var self = this;
     self.installService = installService;
     self.updateService = updateService;
@@ -15,7 +15,7 @@ function($rootScope, toast, confirm, logger, updateService, installService, moda
         modalService.open('isoDialogController', 'js/install/iso-dialog.html')
             .then((iso) => {
                 logger.debug('Selected iso', iso);
-                self.installService.installConfig.iso = iso;
+                installService.installConfig.iso = iso;
             })
             .catch(() => { /* handle rejection */ });
     };
@@ -24,33 +24,33 @@ function($rootScope, toast, confirm, logger, updateService, installService, moda
         modalService.open('driveController', 'js/install/drive-dialog.html')
             .then(function(drive) {
                 logger.debug('Selected drive', drive);
-                self.installService.installConfig.drive = drive;
+                installService.installConfig.drive = drive;
             })
             .catch(() => { /* handle rejection */ });
     };
 
     self.openWifiDialog = function() {
         modalService.open('wifiController', 'js/install/wifi-dialog.html', {
-            network: self.installService.installConfig.network,
+            network: installService.installConfig.network,
         })
             .then(function(wifi) {
                 logger.debug('Selected wifi', wifi);
-                self.installService.installConfig.wifi = wifi;
+                installService.installConfig.wifi = wifi;
             })
             .catch(() => { /* handle rejection */ });
     };
 
     self.startInstall = function() {
-        if (!self.installService.installConfig.iso || !self.installService.installConfig.drive) {
+        if (!installService.installConfig.iso || !installService.installConfig.drive) {
             toast.error('Please select a Cleep version and a drive');
             return;
         }
-        if (self.installService.installConfig.network !== 0 && !self.installService.installConfig.wifi) {
+        if (installService.installConfig.network !== 0 && !installService.installConfig.wifi) {
             toast.error('Please configure wifi');
             return;
         }
 
-        logger.debug('Installation config', self.installService.installConfig);
+        logger.debug('Installation config', installService.installConfig);
         
         confirm.open('Confirm installation?', 'Installation will erase all drive content. This operation cannot be reversed!<br/> \
                       Please note that admin permissions will be requested after file download.', 'Yes, install', 'No')
@@ -63,4 +63,12 @@ function($rootScope, toast, confirm, logger, updateService, installService, moda
     self.cancelInstall = function() {
         installService.cancelInstall();
     };
+
+    self.downloadIso = function() {
+        if (!installService.installConfig.iso.url) {
+            return;
+        }
+
+        downloadService.downloadUrl(installService.installConfig.iso.url);
+    }
 }]);
