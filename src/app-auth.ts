f7f540e12995846284baeb2a68d667457932040f
Tangb <tanguy.bonneau@gmail.com>
2023-03-25 20:25:05 +0100

    Purge stored auth credentials after a while

--- src/app-auth.ts
+++ src/app-auth.ts
@@ -3,9 +3,11 @@ import { appLogger } from './app-logger';
 import { sendDataToAngularJs } from './utils/ui.helpers';
 
 export interface IAuth {
+  deviceUuid: string;
   account: string;
   password: string;
   attempts: number;
+  timestamp: number;
 }
 
 export interface IAuthEvent {
@@ -16,26 +18,51 @@ export interface IAuthEvent {
 }
 
 export const MAX_AUTH_ATTEMPTS = 5;
+const TTL = 30 * 60 * 1000; // 30 mins in ms
+const PURGE_DELAY = 5 * 60 * 1000; // 5 mins in ms
 
 class AppAuth {
-  private auth: Record<string, IAuth> = {};
+  private auths: Record<string, IAuth> = {};
   private window: BrowserWindow;
 
   public configure(window: BrowserWindow): void {
     this.window = window;
     this.addIpcs();
+
+    setInterval(this.purgeObsoleteAuths.bind(this), PURGE_DELAY);
   }
 
   public getAuth(url: string): IAuth {
-    if (this.auth[url]) {
-      this.auth[url].attempts += 1;
+    if (this.auths[url]) {
+      this.auths[url].attempts += 1;
     }
-    return this.auth[url];
+    return this.auths[url];
   }
 
   public resetAuthAttempts(url: string): void {
-    if (this.auth[url]?.attempts) {
-      this.auth[url].attempts = 0;
+    if (this.auths[url]?.attempts) {
+      this.auths[url].attempts = 0;
+    }
+  }
+
+  private purgeObsoleteAuths(): void {
+    const now = new Date().valueOf();
+    const authUrlToDelete = Object.entries(this.auths).reduce((acc, [url, auth]) => {
+      if (now >= auth.timestamp + TTL) {
+        acc.push(url);
+      }
+      return acc;
+    }, []);
+
+    for (const url of authUrlToDelete) {
+      appLogger.debug(`Purging auth for url ${url}`);
+
+      sendDataToAngularJs(this.window, 'device-auth-updated', {
+        deviceUuid: this.auths[url].deviceUuid,
+        hasAuthStored: false,
+      });
+
+      delete this.auths[url];
     }
   }
 
@@ -44,10 +71,12 @@ class AppAuth {
       appLogger.debug('Add new auth data', auth);
 
       const url = new URL(auth.url);
-      this.auth[url.hostname] = {
+      this.auths[url.hostname] = {
         account: auth.account,
         password: auth.password,
         attempts: 0,
+        timestamp: new Date().valueOf(),
+        deviceUuid: auth.deviceUuid,
       };
 
       // return back device has auth to angular
